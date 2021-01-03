5c4a60f4e1944a7f9f57c7f21ee1d8852b1ff427
johnche <john20chen@gmail.com>
2021-01-03 22:55:17 +0100

    also draw samples

--- scripts/modules/Visualizer.js
+++ scripts/modules/Visualizer.js
@@ -1,18 +1,22 @@
 export default class Visualizer {
 	constructor(analyser) {
-		this.canvas = document.getElementById('audio_visual');
-		this.ctx = this.canvas.getContext("2d");
-
-		//analyser.fftSize = 2048;
+		analyser.fftSize = 2048; // this should be default, but just in case...
 		this.analyser = analyser;
 		this.animationId = window.requestAnimationFrame(this.visualizerLoop);
 		this.frequencyBuffer = new Uint8Array(analyser.frequencyBinCount);
+		this.samplesBuffer = new Uint8Array(analyser.fftSize);
+
+		this.canvas = document.getElementById('audio_visual');
+		this.ctx = this.canvas.getContext("2d");
+		this.centerX = this.canvas.width/2;
+		this.centerY = this.canvas.height/2;
 		this.space = this.canvas.width/this.frequencyBuffer.length;
 	}
 
 	visualizerLoop = () => {
 		this.animationId = window.requestAnimationFrame(this.visualizerLoop);
 		this.analyser.getByteFrequencyData(this.frequencyBuffer);
+		this.analyser.getByteTimeDomainData(this.samplesBuffer);
 		this.render();
 	};
 
@@ -22,6 +26,14 @@ export default class Visualizer {
 
 	render = () => {
 		this.clear();
+
+		this.samplesBuffer.forEach((value, i) => {
+			this.ctx.beginPath();
+			this.ctx.moveTo(i, 0);
+			this.ctx.lineTo(i, value);
+			this.ctx.stroke();
+		});
+
 		this.frequencyBuffer.forEach((value, i) => {
 			this.ctx.beginPath();
 			this.ctx.moveTo(this.space*i, this.canvas.height);

a010570f1ace67cd1b078d3b7330025211d6fea5
johnche <john20chen@gmail.com>
2022-02-23 23:46:10 +0100

    inject dynamically sized (fullscreen) canvas

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,16 @@ import FeldmanMachine from './modules/FeldmanMachine.js';
 import MediaHelper from './modules/MediaHelper.js';
 
 const main = async () => {
+	const canvas = document.createElement("canvas");
+	canvas.id = 'audio_visual';
+	canvas.className = 'center';
+	canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight;
+	document.body.appendChild(canvas);
+
 	const htmlElements = {
+		canvas,
 		progressBar: document.getElementById("progressBar"),
-		canvas: document.getElementById('audio_visual'),
 		visualizer: document.getElementById("visualizer")
 	}
 

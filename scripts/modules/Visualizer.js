d36cca530c770d4cf8f1cd11f044bd9abe23f2d8
pellepropell <19978264+pbarrattdue@users.noreply.github.com>
2022-02-24 18:07:57 +0100

    changed order

--- scripts/modules/Visualizer.js
+++ scripts/modules/Visualizer.js
@@ -28,7 +28,7 @@ export default class Visualizer {
 		const frequencyBuffer = new Uint8Array(analyser.frequencyBinCount);
 		const samplesBuffer = new Uint8Array(analyser.fftSize);
 
-		this.visualizerIterator = getIterator([carpet2P, carpet4P, vis3, carpetCentre3, vis1, fillDot], true);
+		this.visualizerIterator = getIterator([carpet2P, carpet4P, vis1, carpetCentre3, vis3, fillDot], true);
 		this.visualizer = this.visualizerIterator.next().value;
 		this.colorThemeIterator = getIterator(shuffleArray([...colors]), true);
 		const { colorList, background } = this.colorThemeIterator.next().value;

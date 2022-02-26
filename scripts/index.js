77d0bbf16d2e01402fa0a44dac314897bb0870f2
johnche <john20chen@gmail.com>
2022-02-26 02:56:06 +0100

    add descriptive text under canvas

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,10 @@ import FeldmanMachine from './modules/FeldmanMachine.js';
 import MediaHelper from './modules/MediaHelper.js';
 
 const main = async () => {
-	const canvas = document.createElement("canvas");
+	const canvas = document.createElement('canvas');
 	canvas.id = 'audio_visual';
-	canvas.className = 'center';
 	canvas.width = document.documentElement.clientWidth;
-	canvas.height = document.documentElement.clientHeight;
+	canvas.height = 0.98*document.documentElement.clientHeight;
 	document.body.appendChild(canvas);
 
 	const htmlElements = {
@@ -22,8 +21,6 @@ const main = async () => {
 	const feldmanMachine = new FeldmanMachine(htmlElements, mediaHelper);
 
 	const onClick = 'ontouchstart' in window ? 'touchstart' : 'click';
-
-	//document.getElementById("new").onclick = rothkoUpdate;
 	document.body.addEventListener(onClick, feldmanMachine.click, true);
 };
 
